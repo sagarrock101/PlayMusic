2c3ce30dacee5c5b4d3e6eb12c436962359a75f7
Sagar <sagar.mountblue@embibe.com>
2020-09-27 18:41:18 +0530

    Using request permission not ActivityAppCompat.requestPermission() for requesting user permission to read external storage

--- app/src/main/java/com/sagaRock101/playmusic/ui/fragment/ListOfSongsFragment.kt
+++ app/src/main/java/com/sagaRock101/playmusic/ui/fragment/ListOfSongsFragment.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import com.sagaRock101.playmusic.MyApplication
@@ -13,12 +11,11 @@ import com.sagaRock101.playmusic.R
 import com.sagaRock101.playmusic.databinding.FragmentListOfSongsBinding
 import com.sagaRock101.playmusic.model.Song
 import com.sagaRock101.playmusic.ui.adapter.SongAdapter
-import com.sagaRock101.playmusic.utils.Utils
 import com.sagaRock101.playmusic.viewModel.MyViewModelFactory
 import com.sagaRock101.playmusic.viewModel.SongViewModel
 import javax.inject.Inject
 
-class ListOfSongsFragment : BaseFragment<FragmentListOfSongsBinding>()  {
+class ListOfSongsFragment : BaseFragment<FragmentListOfSongsBinding>() {
     val MY_PERMISSION_REQUEST = 1
     val TAG = this.javaClass.name
 
@@ -35,8 +32,7 @@ class ListOfSongsFragment : BaseFragment<FragmentListOfSongsBinding>()  {
                 android.Manifest.permission.READ_EXTERNAL_STORAGE
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            ActivityCompat.requestPermissions(
-                activity!!,
+            requestPermissions(
                 arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
                 MY_PERMISSION_REQUEST
             )
@@ -44,7 +40,7 @@ class ListOfSongsFragment : BaseFragment<FragmentListOfSongsBinding>()  {
             viewModel.getSongs()
         }
     }
-    
+
     override fun initFragmentImpl() {
         songsObserver()
     }
@@ -52,7 +48,6 @@ class ListOfSongsFragment : BaseFragment<FragmentListOfSongsBinding>()  {
     private fun songsObserver() {
         viewModel.songsLD.observe(viewLifecycleOwner, Observer { songs ->
             if (!songs.isNullOrEmpty()) {
-                Utils.showToast(requireContext(), "songs")
                 for (song in songs) {
                     Log.e(TAG, "${song.title}")
                 }
@@ -69,15 +64,13 @@ class ListOfSongsFragment : BaseFragment<FragmentListOfSongsBinding>()  {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        Toast.makeText(context!!, "$requestCode ", Toast.LENGTH_SHORT)
-            .show()
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             MY_PERMISSION_REQUEST -> {
-                Toast.makeText(context!!, "permission granted ", Toast.LENGTH_SHORT)
-                    .show()
-                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(context!!, "permission granted ", Toast.LENGTH_SHORT)
-                        .show()
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    viewModel.getSongs()
                 }
             }
         }
